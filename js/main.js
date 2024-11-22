b7f7914be196201790a0a6cc1f56cfe3f5f199d1
Hondone02 <flasheado32@gmail.com>
2024-11-22 12:56:53 -0300

    uso de parseInt

--- js/main.js
+++ js/main.js
@@ -2,9 +2,16 @@ let nombre = "Martín";
 let edad = 40;
 let plataforma = true;
  
+const dni = 30744190;
+
 
 edad = 41;
 
-console.log("Alumno: "+nombre+" Edad: "+edad) ;
+console.log("Alumno: "+nombre+" Edad: "+edad);
 
+let primerNumero = parseInt(prompt("Ingrese el primer numero"));
+let segundoNumero = parseInt(prompt("Ingrese el segundo numero"));
+
+let resultado = primerNumero + segundoNumero;
+alert("El resultado de la operacion es: "+resultado);
 
